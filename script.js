a4ac5966065e6120e403f434a27a8772f293db34
SajalDevnath <63283752+SajalDevnath@users.noreply.github.com>
2024-07-17 17:13:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,38 @@ const inputs = document.querySelectorAll('.code');
 
 inputs.forEach((input, index) => {
     input.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && input.value === '') {
-            if (index !== 0) inputs[index - 1].focus();
+        if (e.key === 'Backspace') {
+            if (input.value === '') {
+                if (index !== 0) inputs[index - 1].focus();
+            } else {
+                input.value = '';
+            }
+        } else if (e.key >= '0' && e.key <= '9') {
+            input.value = '';
         }
     });
+    
     input.addEventListener('input', (e) => {
         const value = e.target.value;
         if (value) {
-            if (index !== inputs.length - 1) inputs[index + 1].focus();
+            if (index !== inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
         }
+    });
+
+    input.addEventListener('paste', (e) => {
+        e.preventDefault();
+        const pasteData = e.clipboardData.getData('text');
+        const pasteArray = pasteData.split('').slice(0, 6);
+
+        pasteArray.forEach((char, i) => {
+            if (inputs[index + i]) {
+                inputs[index + i].value = char;
+                if (index + i < inputs.length - 1) {
+                    inputs[index + i + 1].focus();
+                }
+            }
+        });
     });
 });
